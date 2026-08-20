e069aae7ac69334a6e52fa890262707036457a6f
hajuanas92-code <hajuanas92@gmail.com>
2026-08-20 11:04:28 +0530

    Connected to database

--- demo/src/main/java/com/example/demo/project/Book.java
+++ demo/src/main/java/com/example/demo/project/Book.java
@@ -1,24 +1,34 @@
 package com.example.demo.project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // creates a book table in database
 public class Book {
-    private int id;
+
+    @Id // defines primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increaments ids (1,2,3..)
+    private Long id;
     private String title;
     private String author;
     private boolean isAvailable;
 
+    // JPA requires empty constructor
     public Book(){}
 
-    public Book(int id,String title, String author) {
+    public Book(Long id,String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.isAvailable = true;
     }
     // id
-    public int getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
     // title
--- demo/src/main/java/com/example/demo/project/BookController.java
+++ demo/src/main/java/com/example/demo/project/BookController.java
@@ -1,71 +1,65 @@
 package com.example.demo.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
-    List<Book> bookList = new ArrayList<>();
-    int nextId = 1;
+    @Autowired
+    private BookRepository bookRepository; // injecting database service
 
     @PostMapping
     public Book addBook(@RequestParam String title,@RequestParam String author ) {
-        Book newBook = new Book(nextId, title, author);
-        nextId++;
-        bookList.add(newBook);
-        return newBook; 
+        Book newBook = new Book();
+        newBook.setTitle(title);
+        newBook.setAuthor(author);
+        newBook.setAvailable(true);
+        return bookRepository.save(newBook); 
     }
     // get author
     @GetMapping("/author/{id}")
-    public Book author(@PathVariable int id){
-        for (Book n: bookList) {
-            if (n.getId() == id) {
-                return n;
-            }
-        }return null;
+    public String author(@PathVariable Long id){
+        Book book = bookRepository.findById(id).orElse(null);
+        return (book != null) ? book.getAuthor() : "Author not found";
     }
     // all books
     @GetMapping("/all")
     public List<Book> getAllBooks(){
-        return bookList;
+        return bookRepository.findAll();
     }
     // one book
     @GetMapping("/oneBook/{id}")
-    public Book getBook(@PathVariable int id){
-        for (Book n: bookList) {
-            if (n.getId() == id) {
-                return n;
-            }
-        } return null;
+    public Book getBook(@PathVariable Long id){
+        return bookRepository.findById(id).orElse(null);
     }
     // delete
     @DeleteMapping("/delete/{id}")
-    public String deleteBook(@PathVariable int id) {
-        for (Book n: bookList) {
-            if (n.getId() == id) {
-                bookList.remove(n);
-                return "Book with id "+id+"deleted successfully";
-            }
-        }return "Book id not found";
+    public String deleteBook(@PathVariable Long id) {
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+        return "Book with id "+id+" deleted successfully";
+        }
+    return "Book id not found";
     }
+    
     // update
     @PutMapping("/update/{id}")
-    public Book updateBook(@PathVariable int id, @RequestParam String newTitle, @RequestParam String newAuthor) {
-        for (Book n: bookList) {
-            if (n.getId() == id) {
-              n.setTitle(newTitle);
-              n.setAuthor(newAuthor);
-              return n;
-            }
-        }
-        return null;
+    public Book updateBook(@PathVariable Long id, @RequestParam String newTitle, @RequestParam String newAuthor) {
+        Book book = bookRepository.findById(id).orElse(null);
+
+        if (book != null) {
+          book.setTitle(newTitle);
+          book.setAuthor(newAuthor);
+          return bookRepository.save(book);
+          
+        } return null;
     }
     // pdf uploading
     @PostMapping("/upload-pdf")
